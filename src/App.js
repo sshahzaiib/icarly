c3c9f1f37e7fcf7187687f4f210603db6f897bd2
Shahzaib <zaibansari20@gmail.com>
2022-03-16 00:14:24 +0500

    Initial Setup -- Mateial UI -- Initial Landing Page design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Box, Container, Grid } from "@mui/material";
+import Banner from "./assets/images/banner.webp";
+import BannerMobile from "./assets/images/banner-mobile.png";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
+  const sm = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Grid container spacing={3}>
+              <Grid item md={9} xs={12}>
+                <Box
+                  sx={{
+                    backgroundImage: `url(${sm ? BannerMobile : Banner})`,
+                    backgroundPosition: "center 10%",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    borderRadius: 10,
+                  }}
+                  height={350}
+                  width="100%"
+                />
+              </Grid>
+              <Grid item sm={3}></Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
---
+++ src/theme/index.js
@@ -0,0 +1,51 @@
+export const themeOptions = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#fec72f',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  overrides: {
+    MuiSwitch: {
+      root: {
+        width: 42,
+        height: 26,
+        padding: 0,
+        margin: 8,
+      },
+      switchBase: {
+        padding: 1,
+        '&$checked, &$colorPrimary$checked, &$colorSecondary$checked': {
+          transform: 'translateX(16px)',
+          color: '#fff',
+          '& + $track': {
+            opacity: 1,
+            border: 'none',
+          },
+        },
+      },
+      thumb: {
+        width: 24,
+        height: 24,
+      },
+      track: {
+        borderRadius: 13,
+        border: '1px solid #bdbdbd',
+        backgroundColor: '#fafafa',
+        opacity: 1,
+        transition: 'background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+      },
+    },
+  },
+  props: {
+    MuiTooltip: {
+      arrow: true,
+    },
+  },
+};
